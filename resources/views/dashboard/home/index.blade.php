a14f59b185e26955fd433f236864abfb1f54882b
Marcos <marcos@3layers.com>
2022-01-15 14:44:04 -0300

    Add FontAwesome

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -5,6 +5,14 @@
         </h2>
     </x-slot>
 
+    <x-slot name="asidebar">
+        <ul>
+            <li>
+                a<i class="fa-solid fa-flag"></i>
+            </li>
+        </ul>
+    </x-slot>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -9,9 +9,11 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
@@ -29,7 +31,7 @@
         </div>
         <div style="min-height: calc(100vh - 120px);" class="w-full flex flex-row">
             <aside class="w-1/5 bg-slate-800 px-4 py-4 text-white">
-                lateral
+                {{ $asidebar }}
             </aside>
             <main class="w-4/5">
                 {{ $slot }}
